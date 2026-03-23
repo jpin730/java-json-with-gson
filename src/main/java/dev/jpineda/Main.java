7d935e69a92185e9a524076f44832fdddffe1907
Jaime Pineda <jpin730@pm.me>
2026-03-22 23:37:54 -0600

    feat: :sparkles: implement Person class and serialize Person object to JSON in main class

--- src/main/java/dev/jpineda/Main.java
+++ src/main/java/dev/jpineda/Main.java
@@ -1,17 +1,15 @@
 package dev.jpineda;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.google.gson.Gson;
+
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        Person person = new Person("John Doe", 30, true);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        Gson gson = new Gson();
+
+        String jsonPerson = gson.toJson(person);
+
+        System.out.println("jsonPerson = " + jsonPerson);
     }
 }
---
+++ src/main/java/dev/jpineda/Person.java
@@ -0,0 +1,46 @@
+package dev.jpineda;
+
+public class Person {
+    private String name;
+    private Integer age;
+    private boolean isStudent;
+
+    public Person(String name, Integer age, boolean isStudent) {
+        this.name = name;
+        this.age = age;
+        this.isStudent = isStudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public boolean isStudent() {
+        return isStudent;
+    }
+
+    public void setStudent(boolean student) {
+        isStudent = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", isStudent=" + isStudent +
+                '}';
+    }
+}
